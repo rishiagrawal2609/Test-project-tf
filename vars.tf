280ab126ffdf5c5d5d0f420ad5d262866c4574a6
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-03-13 17:17:59 +0530

    aws

--- vars.tf
+++ vars.tf
@@ -1,10 +1,5 @@
-variable "region" {
+variable "subnet" {
   description = "Zone on which the VM should be provisioned"
   type = string
-<<<<<<< HEAD
-  source = compute.availabilityzone.list_availabilityzone
+  source = subnet.list_subnet
 }
-=======
-  source = availabilityzone.list_availabilityzone
-}
->>>>>>> 032e5d3e79a1591b5c78471a122f0d86a9a3064e
