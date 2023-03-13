827ef5cb44f6a3028bc6f9eec12a03e0f797eee9
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-03-13 17:11:21 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "region" {
   description = "Zone on which the VM should be provisioned"
   type = string
-  source = azure.availabilityzone.list_availabilityzone
+  source = availabilityzone.list_availabilityzone
 }
