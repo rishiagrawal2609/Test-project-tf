032e5d3e79a1591b5c78471a122f0d86a9a3064e
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-03-13 17:12:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
 
 output "test-case" {
